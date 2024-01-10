3b6ee90d5c2c5178901e5b95a612a768011de861
Shim-UNBO <60973930+Shim-UNBO@users.noreply.github.com>
2024-01-10 17:44:16 +0900

    [Gold IV] Title: 로또, Time: 264 ms, Memory: 66024 KB -BaekjoonHub

--- 백준/Gold/2758. 로또/로또.java
+++ 백준/Gold/2758. 로또/로또.java
@@ -1,40 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args)  {
+       lotto();
+    }
+
+    public static void lotto() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int testCase = Integer.parseInt(br.readLine());
-
-        while(testCase-- > 0){
-            int N, M;
-            StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
-            
-            N = Integer.parseInt(stringTokenizer.nextToken());
-            M = Integer.parseInt(stringTokenizer.nextToken());
-
-            long[][] dp = new long[N+1][M+1];
-
-            for(int i = 0 ; i < M+1; i++){
-                dp[0][i] = 1;
+        try {
+            int testCase = Integer.parseInt(br.readLine());
+            while (testCase --> 0) {
+                StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+                int cnt,maxNum;
+                cnt = Integer.parseInt(stringTokenizer.nextToken());
+                maxNum = Integer.parseInt(stringTokenizer.nextToken());
+                System.out.println(countLottoCombinations(cnt,maxNum));
             }
-
-            for(int n = 1; n < N+1 ; n++){
-                for(int m = 1; m < M+1 ; m++){
-                    dp[n][m] = dp[n-1][m/2] + dp[n][m-1];
-                }
-            }
-
-            System.out.println(dp[N][M]);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
+    public static long countLottoCombinations(int N, int M) {
+        long[][] dp = new long[N + 1][M + 1];
+
+        for (int i = 0; i < M + 1; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int n = 1; n < N + 1; n++) {
+            for (int m = 1; m < M + 1; m++) {
+                dp[n][m] = dp[n - 1][m / 2] + dp[n][m - 1];
+            }
+        }
+
+        return dp[N][M];
+    }
 }
+
+
