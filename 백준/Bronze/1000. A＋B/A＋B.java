fb15268182147cca29058a72b67f28c2049452de
Shim-UNBO <60973930+Shim-UNBO@users.noreply.github.com>
2024-03-19 19:09:09 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            System.out.print(N+M);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+}
