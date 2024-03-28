afdcbe975df17e22f9d1a0e085671047349e4ab9
Shim-UNBO <60973930+Shim-UNBO@users.noreply.github.com>
2024-03-28 19:22:04 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 128 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input;
+            while ((input = br.readLine()) != null && !input.isEmpty()) {
+                StringTokenizer st = new StringTokenizer(input);
+                if (st.hasMoreTokens()){
+                    System.out.println(input);
+                }
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
