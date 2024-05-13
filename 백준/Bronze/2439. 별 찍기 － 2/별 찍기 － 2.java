9aa0b7df084fc1f2b1b5dd00775fc2c1ac300b5e
Shim-UNBO <60973930+Shim-UNBO@users.noreply.github.com>
2024-05-14 03:00:52 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 308 ms, Memory: 20360 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int input = Integer.parseInt(br.readLine());
+            for (int i = 0; i<input; i++){
+                for (int k=0;k<input-1-i;k++){
+                    System.out.print(" ");
+                }
+                for (int j=0; j<i+1;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
